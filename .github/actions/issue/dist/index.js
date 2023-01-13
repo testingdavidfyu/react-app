1e663d7b698653aa186817e453ad7eee1403e115
David Yu <david.f.yu@outlook.com>
2023-01-13 10:15:41 -0500

    commits this

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -5419,29 +5419,33 @@ Object.defineProperty(exports, "toPlatformPath", { enumerable: true, get: functi
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-try {
-    const token = core.getInput('token');
-    const title = core.getInput('title');
-    const body = core.getInput('body');
-    const assignees = core.getInput('assignees');
+async function run() {
+    try {
+        const token = core.getInput('token');
+        const title = core.getInput('title');
+        const body = core.getInput('body');
+        const assignees = core.getInput('assignees');
 
-    const octokit = new github.getOctokit(token);
+        const octokit = new github.getOctokit(token);
 
-    const response = octokit.rest.issues.create({
-        // owner: github.context.repo.owner,
-        // repo: github.context.repo.repo,
-        ...github.context.repo,
-        title,
-        body,
-        assignees: assignees ? assignees.split(',') : undefined
-    });
+        const response = await octokit.rest.issues.create({ // ASYNC FUNCTION
+            // owner: github.context.repo.owner,
+            // repo: github.context.repo.repo,
+            ...github.context.repo,
+            title,
+            body,
+            assignees: assignees ? assignees.split(',') : undefined
+        });
 
-    core.setOutput('issue', JSON.stringify(response.data));
+        core.setOutput('issue', JSON.stringify(response.data));
 
 
-} catch(error) {
-    core.setFailed(error.message);
+    } catch(error) {
+        core.setFailed(error.message);
+    }
 }
+
+run();
 
 /***/ }),
 
